adebbb8420d7ca6feb44ea61a6be1c336a3d641d
zianwar <anwaar.ziani@gmail.com>
2015-01-01 19:17:00 +0000

    add Reminder model

--- src/models/Reminder.java
+++ src/models/Reminder.java
@@ -5,10 +5,51 @@
  */
 package models;
 
+import java.sql.Date;
+
 /**
  *
  * @author zianwar
  */
 public class Reminder {
+    
+    int id;
+    String description;
+    Date rappel;
+    Patient patient;
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getRappel() {
+        return rappel;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setRappel(Date rappel) {
+        this.rappel = rappel;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    
     
 }
