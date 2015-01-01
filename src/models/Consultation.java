af5a2cce38218d4127d13587992f52fc01bbcc46
Saad El Hajjaji <saadelha@gmail.com>
2015-01-01 19:28:59 +0000

    Add some models

--- src/models/Consultation.java
+++ src/models/Consultation.java
@@ -5,10 +5,76 @@
  */
 package models;
 
+import java.sql.Date;
+
 /**
  *
  * @author zianwar
  */
 public class Consultation {
+    private int consultationId;
+    private String description;
+    private Date consultationDate;
+    private String status;
+    private int prix;
+    private Patient patient;
+    private PatientInfo patientInfo;
+
+    public int getConsultationId() {
+        return consultationId;
+    }
+
+    public void setConsultationId(int consultationId) {
+        this.consultationId = consultationId;
+    }
+
+    public String getConsultationDesc() {
+        return description;
+    }
+
+    public void setConsultationDesc(String consultationDesc) {
+        this.description = consultationDesc;
+    }
+
+    public Date getConsultationDate() {
+        return consultationDate;
+    }
+
+    public void setConsultationDate(Date consultationDate) {
+        this.consultationDate = consultationDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public PatientInfo getPatientInfo() {
+        return patientInfo;
+    }
+
+    public void setPatientInfo(PatientInfo patientInfo) {
+        this.patientInfo = patientInfo;
+    }
+    
     
 }
--- src/models/Payment.java
+++ src/models/Payment.java
@@ -5,10 +5,49 @@
  */
 package models;
 
+import java.sql.Date;
+
 /**
  *
  * @author zianwar
  */
 public class Payment {
+    private int paymentId;
+    private int amount;
+    private Date paymentDate;
+    private Patient patient;
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    
     
 }
