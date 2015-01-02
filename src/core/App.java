b25abdeb969a1cede558717c50ccaf2513a2525d
zianwar <anwaar.ziani@gmail.com>
2015-01-02 22:23:59 +0000

    delete comments

--- src/core/App.java
+++ src/core/App.java
@@ -39,57 +39,18 @@ public class App {
     
     public static void runApp() {
 
-        /*
-         * Create the model. It's a bit artificial to only be
-         * creating one model class called "Model". You may
-         * or may not have such a class. The important thing
-         * is to have a package called "model" or something 
-         * along those lines, where you put your model code, 
-         * however many classes are in the model.
-         */
         Model model = new Model();
-
-        /*
-         * Once again, you'll likely have a "view" package, not
-         * just one actual class called "View".
-         * 
-         * The view here listens to the model, but some
-         * implementations of MVC do not allow the view to
-         * touch the model in any way, rather only allowing
-         * the controller package classes to interact with
-         * model package classes.
-         */
-//        View view = new View(model);
-
-        /*
-         * Finally, the controller; once again, this should be
-         * a package, and you may even have multiple controllers,
-         * or one controller and many sub-controllers, etc.
-         * But for many apps, there will just be one controller
-         * which deals with routing all messages; it listens
-         * to the view and model and instructs both what to do.
-         */
-//        Controller controller = new Controller(view, model);
-
-        /*
-         * The controller must listen to the view to find
-         * out what happens in it. The controller should
-         * not be concerned with what actually happens in the
-         * view in terms of buttons and other widgets, but only
-         * a high-level view of instructions coming from the view
-         * (save such-and-such, adjust such-and-such, shutdown the app,
-         * etc).
-         */
+        View view = new View(model);
+        Controller controller = new Controller(view, model);
         
         UserController userController = new UserController();
         
         LoginFrame loginFrame = new LoginFrame();
         loginFrame.setVisible(true);
-        
         loginFrame.setLoginListener(userController);
         
-//        view.setSaveListener(controller);
-//        view.setAppListener(controller);
+        
+        
 
         /*
          * In this implementation of MVC, the view listens
