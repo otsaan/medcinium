c0e221a8ad771a72fe1ae2a87742dee5039b8b28
zianwar <anwaar.ziani@gmail.com>
2015-01-02 22:23:35 +0000

    update View

--- src/views/View.java
+++ src/views/View.java
@@ -18,25 +18,15 @@ import models.User;
 public class View extends javax.swing.JFrame implements ActionListener {
     
     private Model model;
-   
     
     /**
      * Creates new form View
      */
     public View(Model model) {
-        this.model = model;
-        
-        try {
-            Starter starter = new Starter();
-            Thread.sleep(2000);                 
-            starter.setVisible(false);
-        } catch(InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        }
-        
-        
-        new LoginFrame().setVisible(true);
-        
+        this.model = model; 
+    }
+    
+    public void show() {
         initComponents();
     }
 
@@ -50,23 +40,38 @@ public class View extends javax.swing.JFrame implements ActionListener {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 741, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 573, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
     // End of variables declaration//GEN-END:variables
 
     
