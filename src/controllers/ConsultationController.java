dd873b32f81ce31669074460662d49bbedf9b416
Saad El Hajjaji <saadelha@gmail.com>
2015-01-03 22:36:41 +0000

    Update ConsultationController

--- src/controllers/ConsultationController.java
+++ src/controllers/ConsultationController.java
@@ -5,10 +5,42 @@
  */
 package controllers;
 
+import models.Consultation;
+
 /**
  *
  * @author zianwar
  */
-public class ConsultationController {
+public class ConsultationController implements Observer {
+
+    public void onCreate(Consultation consultation) {
+        System.out.println("Consultation Created!");
+    }
+
+    public void onUpdate(Consultation consultation) {
+        System.out.println("Consultation Updated!");
+    }
+
+    public void onDelete(Consultation consultation) {
+        System.out.println("Consultation Deleted!");
+    }
+    
+    @Override
+    public void execute(Object view, Object dataObj, String action) {
+        if ((dataObj instanceof Consultation)) {
+            
+            if (action.equalsIgnoreCase("create")) {
+                this.onCreate((Consultation)dataObj);
+            }
+
+            if (action.equalsIgnoreCase("update")) {
+                this.onUpdate((Consultation)dataObj);
+            }
+
+            if (action.equalsIgnoreCase("delete")) {
+                this.onDelete((Consultation)dataObj);
+            }
+        }
+    }
     
 }
