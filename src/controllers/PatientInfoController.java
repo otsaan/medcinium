9637ddba5b64598a434f66094f872d0b3b54ac6c
zianwar <anwaar.ziani@gmail.com>
2015-01-03 23:12:21 +0000

    Update PatientInfoController

--- src/controllers/PatientInfoController.java
+++ src/controllers/PatientInfoController.java
@@ -5,10 +5,50 @@
  */
 package controllers;
 
+import models.DAO.DAOFactory;
+import models.PatientInfo;
+
 /**
  *
  * @author zianwar
  */
-public class PatientInfoController {
+public class PatientInfoController implements Observer {
+ 
     
+    private void onCreate(PatientInfo PatientInfo) {
+        System.out.println("PatientInfo Created!");
+        DAOFactory.getPatientInfoDAO().create(PatientInfo);
+    }
+    
+    
+    private void onUpdate(PatientInfo PatientInfo) {
+        System.out.println("PatientInfo Updated!");
+        DAOFactory.getPatientInfoDAO().update(PatientInfo);
+    }
+    
+    
+    private void onDelete(PatientInfo PatientInfo) { 
+        System.out.println("PatientInfo Deleted!");
+        DAOFactory.getPatientInfoDAO().delete(PatientInfo);
+    }
+    
+    @Override
+    public void execute(Object view, Object dataObj, String action) {
+        
+        if ((view instanceof Object) && (dataObj instanceof PatientInfo)) {
+            
+            if (action.equalsIgnoreCase("create")) {
+                this.onCreate((PatientInfo)dataObj);
+            }
+
+            if (action.equalsIgnoreCase("update")) {
+                this.onUpdate((PatientInfo)dataObj);
+            }
+
+            if (action.equalsIgnoreCase("delete")) {
+                this.onDelete((PatientInfo)dataObj);
+            }
+            
+        }
+    }
 }
