423afc0f60a5f5da7867f31d5a37215d29c28a09
otmgue <the_loverboy@hotmail.fr>
2015-01-03 23:10:05 +0000

    Add DrugController

--- src/controllers/DrugController.java
+++ src/controllers/DrugController.java
@@ -5,10 +5,45 @@
  */
 package controllers;
 
+import models.DAO.DAOFactory;
+import models.Drug;
+
 /**
  *
  * @author zianwar
  */
-public class DrugController {
+public class DrugController implements Observer {
+   
+    private void onCreate(Drug drug){
+       DAOFactory.getDrugDAO().create(drug);
+    }
+    
+    private void onUpdate(Drug drug) {
+       DAOFactory.getDrugDAO().update(drug);
+    }
+    
+    private void onDelete(Drug drug) {
+       DAOFactory.getDrugDAO().delete(drug);
+    }
+
+    @Override
+    public void execute(Object view, Object dataObj, String action) {
+        //@todo add condition on view  && (dataObj instanceof Drug)
+        
+        Drug drug = (Drug)dataObj;
+        
+        if(action.equals("create")) {
+            this.onCreate(drug);
+        }
+        
+        if(action.equals("update")) {
+            this.onUpdate(drug);
+        }
+       
+        if(action.equals("delete")) {
+            this.onDelete(drug);
+        }
+  
+    }
     
 }
