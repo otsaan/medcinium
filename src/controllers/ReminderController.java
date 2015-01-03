a3fc4b67e04e23807566d9352fa13d3e62ed576d
zianwar <anwaar.ziani@gmail.com>
2015-01-03 22:55:48 +0000

    add ReminderController

--- src/controllers/ReminderController.java
+++ src/controllers/ReminderController.java
@@ -5,10 +5,47 @@
  */
 package controllers;
 
+import models.Reminder;
+
 /**
  *
  * @author zianwar
  */
-public class ReminderController {
+public class ReminderController implements Observer{
+    
+    
+    private void onCreate(Reminder reminder) {
+        System.out.println("Reminder Created!");
+    }
+    
+    
+    private void onUpdate(Reminder reminder) {
+        System.out.println("Reminder Updated!");
+    }
+    
+    
+    private void onDelete(Reminder reminder) { 
+        System.out.println("Reminder Deleted!");
+    }
+    
+    @Override
+    public void execute(Object view, Object dataObj, String action) {
+        
+        if ((view instanceof Object) && (dataObj instanceof Reminder)) {
+            
+            if (action.equalsIgnoreCase("create")) {
+                this.onCreate((Reminder)dataObj);
+            }
+
+            if (action.equalsIgnoreCase("update")) {
+                this.onUpdate((Reminder)dataObj);
+            }
+
+            if (action.equalsIgnoreCase("delete")) {
+                this.onDelete((Reminder)dataObj);
+            }
+            
+        }
+    }
     
 }
