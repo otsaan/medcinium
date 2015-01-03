6ab0a79dd33ff1e55379ef95723d55deb6009e27
otmgue <the_loverboy@hotmail.fr>
2015-01-03 22:44:27 +0000

    Add AllergyController

--- src/controllers/AllergyController.java
+++ src/controllers/AllergyController.java
@@ -5,10 +5,53 @@
  */
 package controllers;
 
+import models.Allergy;
+import models.DAO.AllergyDAO;
+
 /**
  *
  * @author zianwar
  */
-public class AllergyController {
+public class AllergyController implements Observer  {
+ 
+    private void onCreate(Allergy allergy )
+    {
+        AllergyDAO allergyDAO = new AllergyDAO();
+        allergyDAO.create(allergy);
+    }
+    private void onUpdate(Allergy allergy)
+    {
+      AllergyDAO allergyDAO = new AllergyDAO();
+      allergyDAO.update(allergy);  
+    }
+    
+    private void onDelete( Allergy allergy)
+    {
+        AllergyDAO allergyDAO = new AllergyDAO();
+        allergyDAO.delete(allergy);   
+    }
+    
+    
+    
+    @Override
+    public void execute(Object view, Object dataObj, String action) {
+        //@todo add condition on view  && (dataObj instanceof Allergy)
+        Allergy allergy=(Allergy)dataObj;
+        if(action.equals("create") )
+        {
+          this.onCreate(allergy);
+        }
+        
+         if(action.equals("update") )
+        {
+          this.onUpdate(allergy);
+        }
+         
+          if(action.equals("delete") )
+        {
+          this.onDelete(allergy);
+        }
+    }
+    
     
 }
