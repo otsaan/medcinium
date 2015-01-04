72f08316f8436334ce23460b23bf1e178272757a
zianwar <anwaar.ziani@gmail.com>
2015-01-04 23:24:50 +0000

    add new startup loader

--- src/views/LoginFrame.java
+++ src/views/LoginFrame.java
@@ -7,6 +7,8 @@ package views;
 
 import controllers.Observer;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import models.User;
 
 /**
@@ -17,21 +19,17 @@ public class LoginFrame extends javax.swing.JFrame {
 
     private Vector<Observer> observers;
     
-
     
     public LoginFrame() {
         
         observers = new Vector<Observer>();
         
-        initComponents();
-        
         try {
-           Starter starter = new Starter();
-           Thread.sleep(2000);                 
-           starter.setVisible(false);
-        } catch(InterruptedException ex) {
-           Thread.currentThread().interrupt();
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Loader.class.getName()).log(Level.SEVERE, null, ex);
         }
+        initComponents();
     }
 
     /**
@@ -43,22 +41,27 @@ public class LoginFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        loginButton = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         usernameTextField = new javax.swing.JTextField();
         passwordField = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        loginButton.setText("login");
-        loginButton.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("login");
+
+        usernameTextField.setColumns(12);
+        usernameTextField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                loginButtonActionPerformed(evt);
+                usernameTextFieldActionPerformed(evt);
             }
         });
 
-        usernameTextField.setColumns(12);
-
         passwordField.setColumns(12);
+        passwordField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                passwordFieldActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -69,7 +72,7 @@ public class LoginFrame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(usernameTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(loginButton))
+                    .addComponent(jButton1))
                 .addContainerGap(137, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -80,11 +83,12 @@ public class LoginFrame extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(35, 35, 35)
-                .addComponent(loginButton)
+                .addComponent(jButton1)
                 .addGap(111, 111, 111))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void loginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginButtonActionPerformed
@@ -96,6 +100,14 @@ public class LoginFrame extends javax.swing.JFrame {
         System.out.println("Login Button Clicked!");
         notifyObservers(user);
     }//GEN-LAST:event_loginButtonActionPerformed
+
+    private void usernameTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_usernameTextFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_usernameTextFieldActionPerformed
+
+    private void passwordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_passwordFieldActionPerformed
     
     
     public void addObserver(Observer observer) {
@@ -112,7 +124,7 @@ public class LoginFrame extends javax.swing.JFrame {
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton loginButton;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPasswordField passwordField;
     private javax.swing.JTextField usernameTextField;
     // End of variables declaration//GEN-END:variables
