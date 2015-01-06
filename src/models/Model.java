f35ec4a1bd8f4cd0c42c0eedf6585bc4cd7718c8
zianwar <anwaar.ziani@gmail.com>
2015-01-06 22:33:11 +0000

    Update Model class

--- src/models/Model.java
+++ src/models/Model.java
@@ -5,10 +5,96 @@
  */
 package models;
 
+import java.util.Vector;
+
 /**
  * Model doesn't import anything 
  * @author otsaan
  */
 public class Model {
+    
+    
+    // -------------- this data will be shown on AccueilFrame -------------------
+    // this vectors will pulled directly from the database by the DAOs
+    // so ConsultationDAO will have 2 methods returning these 2 vectors
+    private Vector<Consultation> pendingConsultations; // file d'attente
+    private Vector<Consultation> finishedConsultations; // dernieres visites
+    
+    // this vectors will be provided by the appropriate DAO
+    private Vector<Reminder> reminders; // today's or 3 days reminders it depends...
+    
+    
+    // -------------- this data will be shown on PatientFrame -------------------
+    private Vector<Patient> patients;
+    
+    
+    // -------------- this data will be shown on ConsultationsFrame -------------------
+    private Vector<Consultation> allConsultations; // all types of 
+    
+    
+    // --------------------- this data will be shown on PatientProfileFrame (not yet) ---------------------
+    // Consultations made by a given Patient to show them on his profile 
+    // this consultation should be return by the PatientDAO.getConsultations(id)
+    private Vector<Consultation> patientConsultations;
+    
+    // infromations of a given Patient 
+    private Vector<PatientInfo> patientInfos;
+    
+    // allergies of a given patient
+    private Vector<Allergy> patientAllergies;
+
+    
+    
+    
+    public Model() {
+        load();
+    }
+   
+    /**
+     * Loades all data using the DAOs
+     */
+    private void load() {
+        
+    }
+
+    
+    // ------------------ GETTERS ---------------------
+    
+    public Vector<Consultation> getPendingConsultations() {
+        return pendingConsultations;
+    }
+
+    public Vector<Consultation> getFinishedConsultations() {
+        return finishedConsultations;
+    }
+
+    public Vector<Reminder> getReminders() {
+        return reminders;
+    }
+
+    public Vector<Patient> getPatients() {
+        return patients;
+    }
+
+    public Vector<Consultation> getAllConsultations() {
+        return allConsultations;
+    }
+
+    public Vector<Consultation> getPatientConsultations() {
+        return patientConsultations;
+    }
+
+    public Vector<PatientInfo> getPatientInfos() {
+        return patientInfos;
+    }
+
+    public Vector<Allergy> getPatientAllergies() {
+        return patientAllergies;
+    }
+    
+    
+    
+    
+   
     
 }
