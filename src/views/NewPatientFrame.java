f910cf719e75edf9b20c93bcc63f75aa99ff5842
otmgue <the_loverboy@hotmail.fr>
2015-01-15 19:43:29 +0000

    ADD a fix

--- src/views/NewPatientFrame.java
+++ src/views/NewPatientFrame.java
@@ -53,7 +53,7 @@ public class NewPatientFrame extends javax.swing.JFrame {
         sexChoice = new javax.swing.JComboBox();
         AddButton = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setText("Nom");
 
